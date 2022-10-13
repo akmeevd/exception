0bd3aabebabf807b065b9e8dc382b57f78f7b887
Dima <akmeev2011@yandex.ru>
2022-10-13 12:24:21 +0300

    exception

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
+import Transport.Bus;
+import Transport.Car;
+import Transport.MotorCar;
+
 public class Main {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws WrongLoginException, WrongPasswordException {
+        System.out.println(Data.inputData("akmeev", "akmeev", "akmeev"));
+        MotorCar motorCar = new MotorCar("KIA", "RIO", 2.0);
+        MotorCar motorCar2 = new MotorCar("Skoda", "Rapid", 2.0);
+        Bus bus = new Bus("some", "some", 9.0);
+        Car car = new Car("some", "some", 3.0);
+        Car car2 = new Car("some", "some", 3.0);
+        motorCar.performDiagnostic();
+        System.out.println(motorCar.isDiagnostic());
+        Driver driver = new Driver("paul", true, 2010, Driver.Category.B);
+
+
+
+
+
     }
 }
---
+++ src/Transport/Bus.java
@@ -0,0 +1,26 @@
+package Transport;
+
+import java.util.Objects;
+
+public class Bus extends Car {
+
+    public Bus(String brand, String model, Double engineVolume) {
+        super(brand, model, engineVolume);
+    }
+
+
+
+    public void startMoving() {
+        System.out.println("Я начинаю движение на автобусе");
+    }
+
+    public void finishMoving() {
+        System.out.println("Я остановился на автобусе");
+    }
+
+
+
+
+
+
+}
